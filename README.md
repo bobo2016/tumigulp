935ef59c50f15185db132934f46ee2a9075edb29
Jingbo Zhang <jb.zhang1234@gmail.com>
2016-01-20 17:09:52 +0800

    Add gulp demo code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tumigulp
+
+
+##$ npm install gulp -g
+
+##$ npm install gulp --save-dev
+
+##$ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-clean gulp-notify gulp-rename gulp-livereload gulp-cache --save-dev
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+	sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload');
+
+gulp.task('default',function(){
+	//tasks
+});
+
+gulp.task('sass',function(){
+	return gulp.src('src/styles/main.scss')
+	.pipe(sass({style: 'compressed'}))
+	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+	.pipe(gulp.dest('dist/assets/css'))
+});
+
